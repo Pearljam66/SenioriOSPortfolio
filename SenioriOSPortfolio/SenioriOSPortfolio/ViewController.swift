6bd6a24adf2e66fc00632c1af51c001bbbb5ba21
Sarah <sarahireneclark@gmail.com>
2025-03-26 00:22:25 -0500

    Initial commit of project

---
+++ SenioriOSPortfolio/SenioriOSPortfolio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SenioriOSPortfolio
+//
+//  Created by Sarah Clark on 3/26/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SenioriOSPortfolio/SenioriOSPortfolioTests/SenioriOSPortfolioTests.swift
@@ -0,0 +1,17 @@
+//
+//  SenioriOSPortfolioTests.swift
+//  SenioriOSPortfolioTests
+//
+//  Created by Sarah Clark on 3/26/25.
+//
+
+import Testing
+@testable import SenioriOSPortfolio
+
+struct SenioriOSPortfolioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
